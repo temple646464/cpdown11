03e65deb2a6c16797bf90174a9fbd091c5c9ec03
arzaan <xdarzanplug@gmail.com>
2024-01-28 22:36:49 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 4942197
+api_hash = "13248a2c551b73193969b42194023635"
+bot_token = "6647149829:AAHWx6Wfray3vrq23VvD-0fJGKF3p0Gs6SQ"
+auth_users = [5892781710]
+sudo_user = 5892781710
+log_channel = -1002001856626
+txt_channel = -1002073188657
 
 
