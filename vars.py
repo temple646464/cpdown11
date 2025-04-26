3de2aec6de5e533f5ff31879b715e4f37fb3a290
temple646464 <Temple646464@gmail.com>
2025-04-26 15:17:36 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
-api_id = 4942197
-api_hash = "13248a2c551b73193969b42194023635"
-bot_token = "6647149829:AAHWx6Wfray3vrq23VvD-0fJGKF3p0Gs6SQ"
+api_id = 28748671
+api_hash = "f53ec7c41ce34e6d585674ed9ce6167c"
+bot_token = "7896165148:AAFKmmxHT01cbgqPcuHpbigHnGETVO4vSk4"
 auth_users = [5892781710]
 sudo_user = 5892781710
 log_channel = -1002001856626
